66493cb65330146d4625b7282c24f02ffa4326b6
JoaoTrento <trentojoao01@gmail.com>
2024-10-29 19:12:11 -0300

    Classe Funcoes Criada

---
+++ src/objetos/Funcoes.java
@@ -0,0 +1,10 @@
+package objetos;
+
+import java.util.ArrayList;
+
+public class Funcoes {
+	
+	ArrayList<Vaga> listaDeVagas = new ArrayList<>();
+	ArrayList<Veiculo> listaDeVeiculos = new ArrayList<>();
+
+}
