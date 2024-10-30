633aa27dcb99d2dd757cfa10bc7382411528d66f
JoaoTrento <trentojoao01@gmail.com>
2024-10-30 08:30:10 -0300

    Main Criado

--- src/objetos/Funcoes.java
+++ src/objetos/Funcoes.java
@@ -1,10 +1,21 @@
 package objetos;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Funcoes {
 	
 	ArrayList<Vaga> listaDeVagas = new ArrayList<>();
 	ArrayList<Veiculo> listaDeVeiculos = new ArrayList<>();
+	
+	Scanner scan = new Scanner(System.in);
+	
+	public void horas() {
+		System.out.println("Insira a hora da chegada (hh mm):");
+		int horaDaEntrada = scan.nextInt();
+		int minutosDaEntrada = scan.nextInt();
+		
+		System.out.println("" + horaDaEntrada + ":" + minutosDaEntrada);
+	}
 
 }
---
+++ src/principal/SistemaPrincipal.java
@@ -0,0 +1,10 @@
+package principal;
+
+public class SistemaPrincipal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
